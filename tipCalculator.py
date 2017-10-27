31928796251b8de2d126db6e4c8be251171d8bb3
Luana Gouvea <luanadotcom@gmail.com>
2017-10-27 17:08:27 +0100

    Fix stuff

--- tipCalculator.py
+++ tipCalculator.py
@@ -11,8 +11,8 @@
 tipRate = input("Enter the tip rate: ");
 billAmount = input("Enter the bill amount: ");
 
-tip = str(((tipRate / 100) * billAmount));
-total = str((tip + billAmount));
+tip = ((tipRate / 100.00) * billAmount);
+total = (tip + billAmount);
 
 print ("The tip amount is " + tip);
 print ("The total amount is " + total);
