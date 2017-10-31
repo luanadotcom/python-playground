14c093d2dd5f16b7d4e5ac46ae0dd9259631c9c7
Luana Gouvea <luanadotcom@gmail.com>
2017-10-31 13:41:30 +0000

    Check for the input type

--- tipCalculator.py
+++ tipCalculator.py
@@ -7,9 +7,15 @@
 # expected result:
 # tip
 # total
+import numbers
 
-tipRate = input("Enter the tip rate: ");
-billAmount = input("Enter the bill amount: ");
+tipRate = raw_input("Enter the tip rate: ");
+if not isinstance(tipRate, int):
+    tipRate = input("Please only enter numbers as the tip rate: ");
+
+billAmount = raw_input("Enter the bill amount: ");
+if not isinstance(billAmount, float):
+    billAmount = input("Please only enter number as the bill amount: ")
 
 tip = ((tipRate / 100.00) * billAmount);
 total = (tip + billAmount);
