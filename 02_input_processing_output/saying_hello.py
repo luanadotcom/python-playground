319daf2ae0b3c1632dfe606b410be64d13571f5e
Luana Gouvea <luanadotcom@gmail.com>
2017-12-08 16:30:48 +0000

    Add description of the problem

--- 02_input_processing_output/saying_hello.py
+++ 02_input_processing_output/saying_hello.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python
 
+# output:
+# What is your name _Luana_
+# Hello, Luana, nice to meet you!
+
+# keep the input, string concatenation and output separate
+
+## challenges
+# write a new version of the program without using variables
+# write a version of the program that displays different greetings for different people... 
 
