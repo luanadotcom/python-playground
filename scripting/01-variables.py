6762f9f15cfdc417ab201abc4613d183f6cbef61
Luana Gouvea <luanadotcom@gmail.com>
2018-02-05 22:52:14 +0000

    Playing with data types

---
+++ scripting/01-variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+first_name = "Luana"
+last_name = "Gouvea"
+age = 27
+birth_date = "17/04/1990"
+
+print("My name is %s %s. \nI was born on %s and I'm %s years old" % (first_name, last_name, birth_date, age))
+
---
+++ scripting/03-lists.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+user = [
+    { 'admin': True, 'active': True, 'name': 'Kevin' }
+    { 'admin': True, 'active': True, 'name': 'Kevin' }
+    { 'admin': True, 'active': True, 'name': 'Kevin' }
+    { 'admin': True, 'active': True, 'name': 'Kevin' }
+]
